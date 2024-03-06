3aefdca5323d43ca980874b077b1ba810b1aabf7
TheTzunder30 <leocedsan@campus.monlau.com>
2024-03-06 18:04:28 +0100

    a

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,5 +1,4 @@
 <?php
-// app/controllers/AuthController.php
 
 class AuthController
 {
@@ -10,22 +9,32 @@ class AuthController
 
     public function authenticate()
     {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        if ($this->isValidUser($username, $password)) {
-            // Iniciar sesión
-            session_start();
-            $_SESSION['username'] = $username;
-
-            // Redirigir a la página de inicio
-            header('Location: /blog/');
+        // Check if username and password are set in the POST request
+        if (isset($_POST['username']) && isset($_POST['password'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+    
+            if ($this->isValidUser($username, $password)) {
+                // Iniciar sesión
+                session_start();
+                $_SESSION['username'] = $username;
+    
+                // Redirigir a la página de inicio
+                header('Location: /blog/');
+                exit();
+            } else {
+                // Mostrar mensaje de error
+                $error = "Usuario o contraseña incorrectos";
+            }
         } else {
-            // Mostrar mensaje de error
-            $error = "Usuario o contraseña incorrectos";
-            require_once './app/views/login.php';
+            // Handle the case where username or password is not set
+            $error = "Por favor, ingrese tanto el nombre de usuario como la contraseña";
         }
+    
+        // Include the login view with or without an error message
+        require_once './app/views/login.php';
     }
+    
 
     private function isValidUser($username, $password)
     {
